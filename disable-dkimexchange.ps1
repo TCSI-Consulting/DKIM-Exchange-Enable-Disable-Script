f9b4532c09926ea6944445c0c2da97458f710d85
TCSITom <69649355+TCSITom@users.noreply.github.com>
2022-01-17 16:58:24 -0800

    Update disable-dkimexchange.ps1

--- disable-dkimexchange.ps1
+++ disable-dkimexchange.ps1
@@ -1,5 +1,30 @@
 Add-PSSnapin Microsoft.Exchange.Management.PowerShell.SnapIn
-stop-service MAPILabPop3ConnectorService
+
 Disable-TransportAgent "Exchange Dkimsigner" -confirm:$false
+
+$MSETD = (get-service MSExchangeTransport).dependentservices
+Write-Host "`r`nStopping Services`r`n-----------------`r`n" -f Yellow
+foreach ($service in $MSETD){
+Write-Host "`r`nAnalyzing $($service.name)" -f Yellow
+    if($service.startmode -eq "auto" -or $service.status -eq "Running"){
+        Write-Host "Stopping $($service.name)"
+        stop-service $service.name
+} 
+else{
+"$($service.name) is $($service.state) with the startmode: $($service.startmode)"
+}
+}
+
 Restart-Service MSExchangeTransport
-start-service MAPILabPop3ConnectorService
+
+$MSETD = (get-service MSExchangeTransport).dependentservices
+foreach ($service in $MSETD){
+Write-Host "`r`nAnalyzing $($service.name)" -f Yellow
+if($service.startmode -eq "auto" -or $service.status -eq "Stopped"){
+"Starting $($service.name)"
+start-service $service.name
+} 
+else{
+"$($service.name) is $($service.state) with the startmode: $($service.startmode)"
+}
+}
